5b3c1f863955e38acd7a86f01219639845724fe9
shark-code <69041356+snowKuramasa@users.noreply.github.com>
2026-04-26 15:52:40 +0900

    APIとサービス層を用意、cors設定のサンプル追加

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -1,16 +1,16 @@
 # Be sure to restart your server when you modify this file.
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
+allowed_origins = ENV.fetch("CORS_ALLOWED_ORIGINS", "").split(",").map(&:strip).reject(&:empty?)
+allowed_origins << "http://localhost:5173" if Rails.env.development?
 
-# Read more: https://github.com/cyu/rack-cors
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins(*allowed_origins)
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+    resource "*",
+      headers: :any,
+      methods: %i[get post put patch delete options head],
+      expose: %w[Authorization],
+      credentials: false
+  end
+end
